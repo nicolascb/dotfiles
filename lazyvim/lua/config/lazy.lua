a7841cb722523404c6871e5f497928d775e01e54
Nicolas Barbosa <ndevbarbosa@gmail.com>
2025-04-24 09:14:33 -0300

    chore: update lazyvim

--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -9,7 +9,10 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+    },
     { import = "lazyvim.plugins.extras.lang.go" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
