568804ee08ace5b322db4c27f004f228eae1ce8f
Nicolas Barbosa <ndevbarbosa@gmail.com>
2025-06-20 09:18:01 -0300

    chore: update lazyvim

--- lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,15 @@ return {
   -- -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { 'datsfilipe/vesper.nvim' },
+  {'embark-theme/vim'},
+  {"rebelot/kanagawa.nvim"},
   --
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-frappe",
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
